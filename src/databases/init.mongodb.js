eaa637f5bb0b26d443bb0a0fd62bf36ff4c57850
Hieu <danghieuyt2@gmail.com>
2023-07-23 22:56:37 +0700

    connect database

---
+++ src/databases/init.mongodb.js
@@ -0,0 +1,39 @@
+'use strict'
+
+const mongoose = require('mongoose')
+const { db: {host, port, name} } = require('../configs/config.mongodb')
+const connectStr = `mongodb://${host}:${port}/${name}`
+
+// SINGLETON PATTERN CONNECT DATABASES
+class Database {
+
+    constructor () {
+        this.connect()
+    }
+
+    connect (type = 'mongodb') {
+
+        // Set log query db in dev invironment
+        if (1 === 1) {
+            mongoose.set('debug', true)
+            mongoose.set('debug', {color: true})
+        }
+
+        // Connect mongodb
+        mongoose.connect(connectStr)
+        .then(() => console.log('Connect database successfully! (SINGLETON PATTERN)'))
+        .catch(err => console.log('Error connect!'))
+    }
+
+    static getInstance() {
+        if(!Database.instance) {
+            Database.instance = new Database()
+        }
+
+        return Database.instance
+    }
+
+}
+
+const intanceMongodb = Database.getInstance()
+module.exports = intanceMongodb
