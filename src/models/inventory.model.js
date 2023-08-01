3a02095b28abeda5ba940630727779f6752805fd
Hieu <danghieuyt2@gmail.com>
2023-08-01 20:59:41 +0700

    add model: inventory (basic level)

---
+++ src/models/inventory.model.js
@@ -0,0 +1,24 @@
+'use strict'
+
+const { model, Schema, Types} = require('mongoose')
+
+const DOCUMENT_NAME = 'Inventory'
+const COLLECTION_NAME = 'Inventories'
+
+const inventorySchema = new Schema({
+    inven_productId: { type: Schema.Types.ObjectId, ref: 'Product' },
+    inven_location: { type: String, default: 'unkhown' },
+    inven_stock: { type: Number, required: true },
+    inven_shopId: { type: Schema.Types.ObjectId, ref: 'Product' },
+    inven_reservations: { type: Array, default: [] },
+    /*
+        cartId: {},
+        stock: {},
+        createdAt: {}
+    */
+}, {
+    timestamps: true,
+    collection: COLLECTION_NAME
+})
+
+module.exports = model(DOCUMENT_NAME, inventorySchema)
