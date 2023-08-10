a67701795618bac791ab2e42692d5cf20e3f8ca7
Hieu <danghieuyt2@gmail.com>
2023-08-10 22:31:41 +0700

    init controller for checkout service

---
+++ src/controllers/checkout.controller.js
@@ -0,0 +1,17 @@
+'use strict'
+
+const CheckoutService = require('../services/checkout.service')
+const { SuccessResponse, OK, CREATED } = require("../core/success.response")
+
+class CheckoutController {
+
+    checkoutReview = async (req, res, next) => {
+        new SuccessResponse({
+            message: 'Get checkout review success!',
+            metadata: await CheckoutService.checkoutReview( req.body )
+        }).send(res)
+    }
+    
+}
+
+module.exports = new CheckoutController()
